5414e98ea6b35e56911a2b8d1bc16b0204f4962c
tippur.sanchitha <tippur.sanchitha@SEMS-259-17.SEMS259.LAB>
2017-11-08 14:49:20 -0800

    Robot Square & my JavaProgram__

---
+++ src/RobotGrafiti.java
@@ -0,0 +1,14 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGrafiti {
+public static void main(String[] args) {
+	Robot    bb8=new Robot ();
+	for(int i=0;i<180; i++) {
+	bb8.move (1);
+	bb8.turn (1);	
+	
+	}
+		
+	}
+}
+
