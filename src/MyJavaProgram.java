62e14b0a9f82e8da07b0b9a7bad94e867967cc9e
tippur.sanchitha <tippur.sanchitha@SEMS-259-17.SEMS259.LAB>
2017-11-08 14:47:49 -0800

    Sani's Project for Java

--- src/MyJavaProgram.java
+++ src/MyJavaProgram.java
@@ -3,11 +3,38 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class MyJavaProgram {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		// START HERE
+	Robot    bb8=new Robot ();
+	bb8.penDown();
+	bb8.move (120);
+	bb8.sparkle();
+	Thread.sleep(1000);
+	bb8.unSparkle();
+	bb8.turn(90);
+	bb8.move (120);
+	bb8.sparkle();
+	Thread.sleep(1000);
+	bb8.unSparkle();
+	bb8.turn(90);
+	bb8.move (120);
+	bb8.sparkle();
+	Thread.sleep(1000);
+	bb8.unSparkle();
+	bb8.turn(90);
+	bb8.move (120);
+	bb8.sparkle();
+	Thread.sleep(1000);
+	bb8.unSparkle();
+	bb8.turn(90);
+	bb8.move (120);
+	bb8.sparkle();
+	Thread.sleep(1000);
+	bb8.unSparkle();
+	Thread.sleep(10000);
 	
-		
 		
 	}
 }
+
